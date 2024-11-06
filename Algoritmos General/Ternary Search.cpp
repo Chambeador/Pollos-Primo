d67ee15e6ea02525724c4c29000cf9764db2c7ca
miguel <miguelcondoriwtf2@gmail.com>
2024-11-05 20:10:40 -0400

    implementacion

---
+++ Algoritmos General/Ternary Search.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <sstream>
+#define int ll
+#define mp make_pair
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define floatigual(a, b) (fabs(a - b) < EPS)
+#define mod(a) ((a) % MOD1)
+#define FOR(i, n) for (int i = 0; i < (n); ++i)
+#define FOR3(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORD(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define FORDD(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vlolo;
+ 
+const int tam = 200010;
+const int MOD = 1000000007;
+const double DINF = 1e100;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+ 
+
+void solve(){
+     cout<<"Implementacion del ternary search";
+     
+}
+signed main(){
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+    solve();
+    return 0;
+}
