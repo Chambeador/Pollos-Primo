5abf85e86ecd5934ce68d77daaafb7f3c494d16c
miguel <miguelcondoriwtf2@gmail.com>
2024-11-07 13:51:58 -0400

    LIS

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -1,40 +1,52 @@
 #include <bits/stdc++.h>
-#include <sstream>
-#define int ll
-#define mp make_pair
-#define pb push_back
-#define all(a) (a).begin(), (a).end()
-#define floatigual(a, b) (fabs(a - b) < EPS)
-#define mod(a) ((a) % MOD1)
-#define FOR(i, n) for (int i = 0; i < (n); ++i)
-#define FOR3(i, a, b) for (int i = (a); i < (b); ++i)
-#define FORD(i, n) for (int i = (n) - 1; i >= 0; --i)
-#define FORDD(i, a, b) for (int i = (b) - 1; i >= (a); --i)
- 
 using namespace std;
  
-typedef long long ll;
-typedef long double ld;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vlolo;
- 
-const int tam = 200010;
-const int MOD = 1000000007;
-const double DINF = 1e100;
-const double EPS = 1e-9;
-const double PI = acos(-1);
- 
 
-void solve(){
-     cout<<"Implementacion del ternary search";
-     
+vector<int> lis(vector<int> &v){
+    int n = v.size();
+    vector<int> dp; 
+    dp.push_back(-1e9); 
+    vector<int> curr(n); 
+    for (int i = 0; i < n; i++) {
+        int l = 0, r = dp.size()-1;
+        int pos = dp.size();
+        while (l <= r) {
+            int m = l + (r-l) / 2;
+            if(dp[m] >= v[i]){
+                pos = m; 
+                r = m - 1; 
+            }else{
+                l = m + 1;
+            }
+        }
+        curr[i] = pos; 
+        if (pos == dp.size()) {
+            dp.push_back(v[i]);
+        } else {
+            dp[pos] = v[i];
+        }
+    }
+    vector<int> ans;
+    int x = dp.size() - 1;
+    for (int i = n - 1; i >= 0; i--) {
+        if (curr[i] == x) {
+            ans.push_back(v[i]);
+            x--;
+        }
+    }
+    reverse(ans.begin(), ans.end()); 
+    return ans;
 }
-signed main(){
-    ios::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
-    solve();
+
+
+signed main() {
+    ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+    int n; cin>>n;vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin>>arr[i];
+    vector<int> ans = lis(arr);
+    cout<<ans.size()<<endl;
+    for(auto x: ans){
+        cout<<x<<" ";
+    }
     return 0;
 }
